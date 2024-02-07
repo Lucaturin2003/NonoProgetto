f9108e90de0838e1c60db44918959b3d3bf26309
Luca Turin <luca@payoff.it>
2024-02-07 09:50:55 +0100

    Prova

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+
+            foreach (var book in cheapBooks)
+                Console.WriteLine(book.Title);
+        }
+    }
+}
+
+public class Book
+{
+    public string Title;
+    public int Price;
+
+    public Book(string title, int price)
+    {
+        Title = title;
+        Price = price;
+    }
+}
+
+public class BookRepository
+{
+    public List<Book> GetBooks() => new()
+    {
+        new Book("Title 1", 100),
+        new Book("Title 2", 5),
+        new Book("Title 3", 100)
+    };
+}
+
